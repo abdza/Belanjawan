5016fb293e1ae30b298ec64ebbc848a673af6043
Abdullah Zainul Abidin <abdullah.zainul@gmail.com>
2011-10-02 07:35:18 +0800

    Added menu to go to manage budget activity

--- src/com/abdullahsolutions/belanjawan/BelanjawanActivity.java
+++ src/com/abdullahsolutions/belanjawan/BelanjawanActivity.java
@@ -1,7 +1,11 @@
 package com.abdullahsolutions.belanjawan;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class BelanjawanActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,23 @@ public class BelanjawanActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.main, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch (item.getItemId()){
+    	case R.id.managebudget:
+    		startActivity(new Intent(this,ManageBudgetActivity.class));
+    		return true;
+    	}
+    	return false;
     }
 }
---
+++ src/com/abdullahsolutions/belanjawan/ManageBudgetActivity.java
@@ -0,0 +1,12 @@
+package com.abdullahsolutions.belanjawan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ManageBudgetActivity extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.managebudget);
+    }
+}

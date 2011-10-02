3960c08e37126d452768c46cae13d67952acc6d0
Abdullah Zainul Abidin <abdullah.zainul@gmail.com>
2011-10-02 23:08:16 +0800

    Added button for input new income and expenses

---
+++ src/com/abdullahsolutions/belanjawan/EditBudgetActivity.java
@@ -0,0 +1,13 @@
+package com.abdullahsolutions.belanjawan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditBudgetActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editbudget);
+    }
+}
